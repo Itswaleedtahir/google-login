6f7c2645c51a9e6f5fbb55f2e3bca788e7cb809f
Waleed Tahir <102904093+Itswaleedtahir@users.noreply.github.com>
2023-01-24 15:46:19 +0500

    login with google

---
+++ google.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+const { SignUp } = require("../models/index");
+const axios = require("axios");
+
+module.exports = {
+  google: async (req, res) => {
+    // var decoded = jwt_decode(req.body.access_token);
+    const result = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${req.body.access_token}`,
+        },
+      }
+    );
+
+    let user = await SignUp.findOne({ googleId: result.data.sub });
+    if (user) {
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+        },
+        process.env.jwtPrivateKey
+      );
+      return res.send(token);
+    } else {
+      const newUser = {
+        googleId: result.data.sub,
+        email: result.data.email,
+        firstname: result.data.given_name,
+        lastname: result.data.family_name,
+      };
+      const user = await SignUp.create(newUser);
+      const token = jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          firstname: user.firstname,
+          lastname: user.lastname,
+        },
+        process.env.jwtPrivateKey
+      );
+      return res.send(token);
+    }
+  },
+};
